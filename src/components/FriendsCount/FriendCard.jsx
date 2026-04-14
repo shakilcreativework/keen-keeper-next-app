41fd20cfad5e13e065c13dd689a313eaf9ea7934
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-14 22:18:06 +0600

    now home page complete

--- src/components/FriendsCount/FriendCard.jsx
+++ src/components/FriendsCount/FriendCard.jsx
@@ -1,26 +1,20 @@
-// {
-// "id": 1,
-// "name": "Amit Rahman",
-// "picture": "https://randomuser.me/api/portraits/men/32.jpg",
-// "email": "amit.rahman@gmail.com",
-// "days_since_contact": 20,
-// "status": "overdue",
-// "tags": ["college", "close friend"],
-// "bio": "We studied computer science together at university. Often go on weekend trips.",
-// "goal": 14,
-// "next_due_date": "2026-04-01"
-// },
 
 import Image from "next/image";
 
-const FriendCard = ({friend}) => {
+const FriendCard = ({ friend }) => {
     // console.log(friend);
-    const {id, name, picture, email, days_since_contact, status, tags, bio, goal, next_due_date} = friend;
+    const { id, name, picture, email, days_since_contact, status, tags, bio, goal, next_due_date } = friend;
 
     return (
-        <div className="p-6 md:p-5 lg:p-6 bg-white rounded-lg flex flex-col justify-center items-center shadow-xs">
+        <div className="p-6 md:p-5 lg:p-6 bg-white rounded-lg flex flex-col justify-center items-center shadow-xs space-y-3">
             <Image src={picture} width={80} height={80} priority className="w-20 rounded-full" alt={name} />
+            <h3 className="text-[#1f2937] text-lg md:text-xl font-semibold">{name}</h3>
             <span className="text-xs text-[#64748b]">{days_since_contact}d ago</span>
+            <div className="flex gap-3">
+                {
+                    tags.map((tag, idx) => <span key={idx} className="bg-[#cbfadb] text-xs  py-2 px-4 rounded-full font-medium uppercase">{tag}</span>)
+                }
+            </div>
             <span className={`text-xs text-white py-2 px-4 rounded-full font-medium capitalize ${status === 'almost due' ? 'bg-[#efad44]' : status === 'overdue' ? 'bg-[#ef4444]' : status === 'on-track' ? 'bg-[#244d3f]' : ''}`}>{status}</span>
         </div>
     );
--- src/components/FriendsCount/FriendsCount.jsx
+++ src/components/FriendsCount/FriendsCount.jsx
@@ -11,20 +11,20 @@ const FriendsCount = () => {
 
     return (
         <div>
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 my-8 lg:my-10 lg:gap-6">
-                <div className="flex flex-col justify-center items-center gap-2 bg-white rounded-md p-6">
-                    <h3 className="text-3xl text-[#244d3f] font-semibold">10</h3>
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-5 my-8 lg:my-10 lg:gap-6">
+                <div className="flex flex-col justify-center items-center gap-2 bg-white rounded-md p-6 text-center">
+                    <h3 className="text-3xl text-[#244d3f] font-semibold">{friends.length}</h3>
                     <p className="text-sm md:text-base lg:text-lg text-[#64748b] font-normal">Total Friends</p>
                 </div>
-                <div className="flex flex-col justify-center items-center gap-2 bg-white rounded-md p-6">
+                <div className="flex flex-col justify-center items-center gap-2 bg-white rounded-md p-6 text-center">
                     <h3 className="text-3xl text-[#244d3f] font-semibold">3</h3>
                     <p className="text-sm md:text-base lg:text-lg text-[#64748b] font-normal">On Track</p>
                 </div>
-                <div className="flex flex-col justify-center items-center gap-2 bg-white rounded-md p-6">
+                <div className="flex flex-col justify-center items-center gap-2 bg-white rounded-md p-6 text-center">
                     <h3 className="text-3xl text-[#244d3f] font-semibold">6</h3>
                     <p className="text-sm md:text-base lg:text-lg text-[#64748b] font-normal">Need Attention</p>
                 </div>
-                <div className="flex flex-col justify-center items-center gap-2 bg-white rounded-md p-6">
+                <div className="flex flex-col justify-center items-center gap-2 bg-white rounded-md p-6 text-center">
                     <h3 className="text-3xl text-[#244d3f] font-semibold">12</h3>
                     <p className="text-sm md:text-base lg:text-lg text-[#64748b] font-normal">Interactions This Month</p>
                 </div>
