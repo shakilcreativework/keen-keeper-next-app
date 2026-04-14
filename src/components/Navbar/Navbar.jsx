36e76d89a9f995f0b64012215f341cadcb411c7e
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-04-14 16:33:22 +0600

    add nav item icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { AiOutlineMenu } from "react-icons/ai";
-import { IoClose } from "react-icons/io5";
+import { IoClose, IoHomeSharp } from "react-icons/io5";
+import { MdAccessTime } from 'react-icons/md';
+import { IoMdStats } from 'react-icons/io';
 
 const Navbar = () => {
     const pathname = usePathname();
@@ -17,28 +19,28 @@ const Navbar = () => {
     const navlinks = <>
         <li>
             <Link
-                className={`${pathname === "/" ? "bg-[#244d3f] text-white" : "text-[#64748b]"} px-4 py-2 rounded-md`}
+                className={`${pathname === "/" ? "bg-[#244d3f] text-white" : "text-[#64748b]"} flex justify-center items-center gap-2 px-4 py-2 rounded-md`}
                 href="/"
             >
-                Home
+               <IoHomeSharp /> Home
             </Link>
         </li>
 
         <li>
             <Link
-                className={`${pathname === "/timeline" ? "bg-[#244d3f] text-white" : "text-[#64748b]"} px-4 py-2 rounded-md`}
+                className={`${pathname === "/timeline" ? "bg-[#244d3f] text-white" : "text-[#64748b]"} flex justify-center items-center gap-2 px-4 py-2 rounded-md`}
                 href="/timeline"
             >
-                Timeline
+               <MdAccessTime /> Timeline
             </Link>
         </li>
 
         <li>
             <Link
-                className={`${pathname === "/stats" ? "bg-[#244d3f] text-white" : "text-[#64748b]"} px-4 py-2 rounded-md`}
+                className={`${pathname === "/stats" ? "bg-[#244d3f] text-white" : "text-[#64748b]"} flex justify-center items-center gap-2 px-4 py-2 rounded-md`}
                 href="/stats"
             >
-                Stats
+               <IoMdStats /> Stats
             </Link>
         </li>
     </>
@@ -68,7 +70,7 @@ const Navbar = () => {
                 {/* small devices */}
                 <ul
                     className={`absolute md:hidden w-full left-0 top-full py-6 px-4 flex flex-col
-                        transition-all duration-300 ease-in-out space-y-5 bg-white gap-2
+                        transition-all duration-300 ease-in-out space-y-5 bg-white gap-1
                         ${open
                             ? "opacity-100 translate-y-0"
                             : "opacity-0 -translate-y-4 pointer-events-none"
